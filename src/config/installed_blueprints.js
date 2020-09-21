f5faad21cef61b04457ddfaa9d9dd29c93c467cd
Crowdbotics <team@crowdbotics.com>
2020-09-21 22:44:39 +0000

    add package blueprint-react-native-tutorial-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'Tutorial4105120', human_name: 'Tutorial 4', access_route: 'Tutorial4105120', icon: 'question-circle'},
 { name: 'UserProfile5105119', human_name: 'User Profile 5', access_route: 'UserProfile5105119'},
 { name: 'Dashboard18105116', human_name: 'Dashboard 1 8', access_route: 'Dashboard18105116'},
 
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,6 +5,7 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
+import Tutorial4105120Navigator from '../features/Tutorial4105120/navigator';
 import UserProfile5105119Navigator from '../features/UserProfile5105119/navigator';
 import Dashboard18105116Navigator from '../features/Dashboard18105116/navigator';
 
@@ -15,6 +16,7 @@ import Dashboard18105116Navigator from '../features/Dashboard18105116/navigator'
 const AppNavigator = {
 
     //@BlueprintNavigationInsertion
+Tutorial4105120: { screen: Tutorial4105120Navigator },
 UserProfile5105119: { screen: UserProfile5105119Navigator },
 Dashboard18105116: { screen: Dashboard18105116Navigator },
 
